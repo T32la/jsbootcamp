427695961123a7732eaf07831b788b44dab3f722
victoriano <mytacam78@gmail.com>
2026-01-13 07:54:38 -0600

    eventos en el navegador con js

---
+++ 02-js/script.js
@@ -0,0 +1,5 @@
+const boton = document.querySelector("#boton-importante");
+
+boton.addEventListener("click", function responseClick() {
+  alert("Gracias por tu interes. Pronto nos pondremos en contacto contigo");
+});
