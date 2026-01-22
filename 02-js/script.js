87c01c978c4bc82ef082dddb6d246c2a88ff6ea4
victoriano <victorianodev@gmail.com>
2026-01-22 17:13:50 -0600

    use class

--- 02-js/script.js
+++ 02-js/script.js
@@ -28,19 +28,26 @@ filter.addEventListener("change", function () {
     mensaje.trextContent = "";
   }
 
-
-  jobs.forEach(job => {
+  jobs.forEach((job) => {
     // const modalidad = job.dataset.modalidad;
     const modalidad = job.getAttribute("data-modalidad");
+    const isShown = selectedValue === "" || selectedValue === modalidad;
 
+    // La mejor forma de utilizar las para las clases en javascript.
+    job.classList.toggle("is-hidden", !isShown);
     // console.log(modalidad);
 
-    if (selectedValue === "" || selectedValue === modalidad) {
-      job.style.display = "flex";
-      // job.classList.remove('is-hidden');
+    /* 
+    * @ Opcion recomendable para trabajar con opciones
+    * se utiliza las clases.
+
+    // if (selectedValue === "" || selectedValue === modalidad) {
+      // job.style.display = "flex";
+      job.classList.remove('is-hidden');
     } else {
-      job.style.display = 'none';
-      // job.classList.add('is-hidden');
-    }
-  })
+      // job.style.display = 'none';
+    job.classList.add('is-hidden');
+  }
+  */
+  });
 });
