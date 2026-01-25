d2c0e424b2010d231dfa45c3b8b14f552c27fa3f
victoriano <victorianodev@gmail.com>
2026-01-25 17:36:27 -0600

    multiples archivos js

--- 02-js/filtros.js
+++ 02-js/filtros.js
@@ -1,53 +1,36 @@
-// Aplicando un evento para muchos eventos
-// delegacion de eventos
-const jobsListingSection = document.querySelector(".jobs-listings");
-const filterTecnology = document.querySelector("#filter-technology");
-const filterUbication = document.querySelector("#filter-location");
-const filterExperience = document.querySelector("#filter-experience-level");
-
-jobsListingSection?.addEventListener("click", function (event) {
-  const element = event.target;
-
-  if (element.classList.contains("button-apply-job")) {
-    element.textContent = "Aplicado!";
-    element.classList.add("is-applied");
-    element.disabled = true;
-  }
-});
-
 const filter = document.querySelector("#filter-location");
 const mensaje = document.querySelector("#filter-selected-value");
 
 filter.addEventListener("change", function () {
+  const jobs = document.querySelectorAll(".job-listing-card");
   const selectedValue = filter.value;
+
   if (selectedValue) {
     mensaje.textContent = `Filtro seleccionado: ${selectedValue}`;
   } else {
     mensaje.trextContent = "";
   }
+
+  jobs.forEach((job) => {
+    // const modalidad = job.dataset.modalidad;
+    const modalidad = job.getAttribute("data-modalidad");
+    const isShown = selectedValue === "" || selectedValue === modalidad;
+
+    // La mejor forma de utilizar las para las clases en javascript.
+    job.classList.toggle("is-hidden", !isShown);
+    // console.log(modalidad);
+
+    /* 
+    * @ Opcion recomendable para trabajar con opciones
+    * se utiliza las clases.
+
+    // if (selectedValue === "" || selectedValue === modalidad) {
+      // job.style.display = "flex";
+      job.classList.remove('is-hidden');
+    } else {
+      // job.style.display = 'none';
+    job.classList.add('is-hidden');
+  }
+  */
+  });
 });
-
-const searchInput = document.querySelector("#empleos-search-input");
-
-searchInput.addEventListener("input", function () {
-  // const inputValue = searchInput.value;
-  console.log(searchInput.value);
-});
-
-searchInput.addEventListener("blur", function () {
-  console.log("Se dispara cuando el campo pierde el foco.");
-});
-
-
-const searchForm = document.querySelector("#empleos-search-form");
-
-searchForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  console.log("submit");
-})
-
-document.addEventListener('keydown', function(event) {
-  console.log("Tecla presionada: ", event.key );
-  console.log("Esta pulsado la tecla shift? ", event.shiftKey);
-  console.log("Esta pulsada la tecla ctrl? ", event.ctrlKey);
-})
