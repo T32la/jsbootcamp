76f345065d1d0d023cd0e82bedea3ef08f2e9071
victoriano <mytacam78@gmail.com>
2026-02-20 11:56:57 -0600

    dev(react) create project vite + javascript

---
+++ 03-react-vite/src/App.jsx
@@ -0,0 +1,25 @@
+import { Header } from "./components/Header.jsx";
+import { SearchFormSection } from "./components/SearchFormSection.jsx";
+import { JobListings } from "./components/JobListings.jsx";
+import { Pagination } from "./components/Pagination.jsx";
+import { Footer } from "./components/Footer.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <SearchFormSection />
+
+        <section>
+          <JobListings />
+          <Pagination />
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ 03-react-vite/src/components/JobListings.jsx
@@ -0,0 +1,9 @@
+export function JobListings() {
+  return (
+    <>
+      <div className="resultado-info" id="resultadoInfo"></div>
+      <h2>Resultados de Busqueda</h2>
+      <div className="jobs-listings"></div>
+    </>
+  );
+}
